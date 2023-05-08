fe4bcdafac0a3582de718a3f8a3001da4d44ee6f
Sergei Gots <Serge.Gots@gmail.com>
2023-05-08 19:35:15 +0300

    Unit test: Initial commit

---
+++ src/test/java/pro/sky/java/util/DenseStringListTest.java
@@ -0,0 +1,68 @@
+package pro.sky.java.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DenseStringListTest {
+
+    @Test
+    void makeExpandable() {
+    }
+
+    @Test
+    void isExpandable() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void lastIndexOf() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void toArray() {
+    }
+}
