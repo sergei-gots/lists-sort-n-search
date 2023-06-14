5d4717647f22d1f385771b0ce2cfded1b04cbaed
Sergei Gots <74458698+Sergei-Gots@users.noreply.github.com>
2023-06-15 02:52:02 +0300

    Update BenchmarkService.java

--- src/main/java/pro/sky/service/BenchmarkService.java
+++ src/main/java/pro/sky/service/BenchmarkService.java
@@ -8,18 +8,36 @@ import pro.sky.util.SortingAndSearchingIntImpl;
 import java.util.Random;
 
 public class BenchmarkService {
+    //used to get index of value for search
     private final static Random random = new Random();
+    //sort-n-search-implementing engine
     SortingAndSearchingInt sortAndSearchEngine = new SortingAndSearchingIntImpl();
+    //Instance of ArrayList's replicating class, stores test data for bench tests
     BenchmarkDenseIntList testList = new BenchmarkDenseIntList();
 
+    /** Performs set of benchmark tests for:
+            - selection sort;
+            - bubble sort;
+            - insertion sort;
+            and then benchmark tests for search a value within test array data:
+            - linear search;
+            - binary search 
+
+            with printing results to the console.
+    **/
     public  void performBenchmark() {
+        //init sort-n-search-engine with test data
         sortAndSearchEngine.loadTestData(testList.toArray());
+        //get random value to search from the data
         int valueToSearch = testList.get(random.nextInt(BenchmarkDenseIntList.TEST_DATA_SIZE));
+
+        //benchmark tests for sorting algorithms
         System.out.println("\n*** Sorting ***");
         benchmarkSort(() -> sortAndSearchEngine.sortSelection(), "Selection sort");
         benchmarkSort(() -> sortAndSearchEngine.sortBubble(), "Bubble sort   ");
         benchmarkSort(() -> sortAndSearchEngine.sortInsertion(), "Insertion sort");
 
+        //benchmark tests for search algorithms
         System.out.println("\n*** Search   ***");
         benchmarkSearch((value) -> sortAndSearchEngine.containsLinear(value), valueToSearch, "Linear search");
         benchmarkSearch((value) -> sortAndSearchEngine.containsBinary(value), valueToSearch, "Binary search");
